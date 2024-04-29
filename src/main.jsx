c102a4278b0fe447284e944b59c13b308c4794aa
Adem Kedir <ademkedir724@gmail.com>
2024-04-29 14:28:14 +0300

    update style for usePopcorn project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+// import Rating from './RatingComponent/Rating.jsx';
+// import TextExpand from './textExpanderComponent/Text-expander.jsx';
+
+/* 
+function Test() {
+  const [moveRate, setMoveRate] = useState(0);
+
+  return (
+    <div>
+      <Rating maxRating={8} onSetRating={setMoveRate} />{' '}
+      <p>the move was rated {moveRate}</p>
+    </div>
+  );
+}
+*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+
+    {/* <Rating maxRating={3} size={11} message={['good', 'perfect', 'amazing']} />
+<Rating maxRating={13} size={22} color="red" defaultRating={3} /> */}
+    {/* <Test /> */}
+
+    {/* <TextExpand /> */}
+  </React.StrictMode>
+);
