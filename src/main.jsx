053d343ef8348c5b257dc026b35f316529ab857e
Adem Kedir <ademkedir724@gmail.com>
2024-04-30 15:53:37 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
-// import Rating from './RatingComponent/Rating.jsx';
-// import TextExpand from './textExpanderComponent/Text-expander.jsx';
+// import './index.css';
+import HowReactWork from './HowReactWork/HowReactWork.jsx';
+import Rating from './RatingComponent/Rating.jsx';
+import TextExpand from './textExpanderComponent/Text-expander.jsx';
 
 /* 
 function Test() {
@@ -21,12 +22,12 @@ function Test() {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-
-    {/* <Rating maxRating={3} size={11} message={['good', 'perfect', 'amazing']} />
-<Rating maxRating={13} size={22} color="red" defaultRating={3} /> */}
+    {/* <App /> */}
+    {/* <Rating maxRating={3} size={11} message={['good', 'perfect', 'amazing']} /> */}
+    {/* <Rating maxRating={13} size={22} color="red" defaultRating={3} /> */}
     {/* <Test /> */}
-
     {/* <TextExpand /> */}
+
+    <HowReactWork />
   </React.StrictMode>
 );
