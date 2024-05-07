0f828fa53c8738f51032fd864af2f551ed0f7dcb
Adem Kedir <ademkedir724@gmail.com>
2024-05-07 17:36:01 +0300

    round number
    - round avgUserRating, avgImdbRating, and TotalRuntime

--- src/App.jsx
+++ src/App.jsx
@@ -346,15 +346,15 @@ function MoviesSummary({ watched }) {
         </p>
         <p>
           <span>⭐️</span>
-          <span>{avgImdbRating}</span>
+          <span>{avgImdbRating.toFixed(1)}</span>
         </p>
         <p>
           <span>🌟</span>
-          <span>{avgUserRating}</span>
+          <span>{avgUserRating.toFixed(1)}</span>
         </p>
         <p>
           <span>⏳</span>
-          <span>{avgRuntime} min</span>
+          <span>{avgRuntime.toFixed(2)} min</span>
         </p>
       </div>
     </div>
