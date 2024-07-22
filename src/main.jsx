14434a52a9c27d53e947b40b1252aa53676472d6
Adem Kedir <ademkedir724@gmail.com>
2024-07-22 16:04:07 +0300

    change main

--- src/main.jsx
+++ src/main.jsx
@@ -20,9 +20,35 @@ function Test() {
 }
 */
 
+// const CountButton = React.memo(function CountButton({ onClick, count }) {
+//   console.log('count button with', count);
+//   return <button onClick={onClick}>{count}</button>;
+// });
+
+function CountButton({ onClick, count }) {
+  console.log('count button with', count);
+  return <button onClick={onClick}>{count}</button>;
+}
+
+function DualCounter() {
+  const [count1, setCount1] = React.useState(0);
+  const increment1 = React.useCallback(() => setCount1((c) => c + 1), []);
+
+  const [count2, setCount2] = React.useState(0);
+  const increment2 = React.useCallback(() => setCount2((c) => c + 1), []);
+
+  return (
+    <>
+      <CountButton count={count1} onClick={increment1} />
+      {/* <button onClick={increment2}>Change {count2}</button> */}
+      <CountButton count={count2} onClick={increment2} />
+    </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DualCounter />
     {/* <Rating maxRating={3} size={11} message={['good', 'perfect', 'amazing']} /> */}
     {/* <Rating maxRating={13} size={22} color="red" defaultRating={3} /> */}
     {/* <Test /> */}
